0068fb8a2db582412c3d342efc2c96553a8e354f
lreed03 <lreed0383@gmail.com>
2025-02-18 16:29:37 -0700

    Created more methods and all the instance variables as well as created the other 6 classes

--- Assignment3/src/edu/ucalgary/oop/DisasterVictim.java
+++ Assignment3/src/edu/ucalgary/oop/DisasterVictim.java
@@ -1,9 +1,19 @@
 package edu.ucalgary.oop;
 
 public class DisasterVictim {
-	final String ENTRY_DATE;
-	String firstName;
-	String dateOfBirth;
+	private String firstName;
+	private String lastName;
+	private String dateOfBirth;
+	private int ASSIGNED_SOCIAL_ID;
+	private FamilyRelation[] familyConnections;
+	private MedicalRecord[] medicalRecords;
+	private Supply[] personalBelongings;
+	private final String ENTRY_DATE;
+	private String gender;
+	private String Comments;
+	private static int counter;
+	
+
 	
 	
 	public DisasterVictim(String firstName, String ENTRY_DATE) {
@@ -11,13 +21,34 @@ public class DisasterVictim {
 		this.firstName = firstName;
 	}
 	
-	public DisasterVictim(String firstName, String ENTRY_DATE, String dateOfBirth) {
+	public DisasterVictim(String firstName, String ENTRY_DATE, String dateOfBirth) throws IllegalArgumentException{
 		this.ENTRY_DATE = ENTRY_DATE;
 		this.firstName = firstName;
 		this.dateOfBirth = dateOfBirth;
 	}
 	
-	String getFirstName() {
+	public String getFirstName() {
 		return this.firstName;
 	}
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
+
+	public String getLastName(){
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName){
+		this.lastName = lastName;
+	}
+
+	public String getDateOfBirth(){
+		return this.dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) throws IllegalArgumentException{
+		// Either throw the exception in only the setter and use the setter in constructor or throw the exception everytime it is set
+		this.dateOfBirth = dateOfBirth;
+	}
+
 }
